a6b3eb0e69d24ebaea74835bd2980d15837e5def
christinaKlaus <klaus.christina@199a.de>
2017-03-31 18:44:28 +0200

    Jump fixed

--- Immerlympia/Assets/Scripts/Playercontroller.cs
+++ Immerlympia/Assets/Scripts/Playercontroller.cs
@@ -89,6 +89,7 @@ public class Playercontroller : MonoBehaviour {
         //Collison with ground
         if (collision.gameObject.tag == "Ground") {
             isJumping = false;
+            Debug.Log("Collision");
             
         } else {
             isJumping = true;
