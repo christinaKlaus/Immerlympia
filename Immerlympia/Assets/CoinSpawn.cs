4e7d41d08a670d2ce253d6004414c1c08e91db22
Chrissi_Surface <klaus.christina@web.de>
2017-04-20 15:08:31 +0200

    Coins spawning everywhere

---
+++ Immerlympia/Assets/CoinSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawn : MonoBehaviour {
+
+	public GameObject coin;
+	public float radius;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SpawnCoin();
+	}
+
+	void SpawnCoin(){
+		float t = 2 * Mathf.PI * Random.value;
+		float u = Random.value + Random.value;
+		float r = ((u>1) ? 2-u : u) * radius;
+		Vector3 pos = new Vector3(r*Mathf.Cos(t), 20, r*Mathf.Sin(t)); 
+		Instantiate(coin, pos, transform.rotation);
+	}
+}
