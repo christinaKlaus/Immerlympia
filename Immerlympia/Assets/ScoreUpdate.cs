11bacc93d5a33698bc9dc866d6377981d560dd0a
christinaKlaus <klaus.christina@199a.de>
2017-06-01 22:44:07 +0200

    Grundlegendes UI (Score), Partikeleffekt auf Coin und Schatten fix
    Schatten durch umstellen aller Assets auf Two Sided gefixt

---
+++ Immerlympia/Assets/ScoreUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdate : MonoBehaviour {
+
+    public GameObject player;
+    Text TextComponent;
+
+    private void Start() {
+        TextComponent = GetComponent<Text>();
+        TextComponent.text = player.ToString() + ": ";
+        TextComponent.text += "" + player.GetComponent<Playercontroller>().score;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        TextComponent.text = player.ToString() + ": ";
+        TextComponent.text += "" + player.GetComponent<Playercontroller>().score;
+    }
+}
