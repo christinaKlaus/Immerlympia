34d487330f8fd2a1a409b5e340e345ab5df2ae41
christinaKlaus <klaus.christina@199a.de>
2017-06-22 18:56:49 +0200

    Anfang Sound System

---
+++ Immerlympia/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	private AudioSource source;
+	public List<AudioClip> clipList;
+
+	void Start () {
+		source = GetComponent<AudioSource>();
+	}
+	
+	public void playClip(AudioClip clip){
+		source.clip = clip;
+		source.Play();
+	}
+
+}
