74848b253811a033086018ea9a6b83c58c8501d7
Paul Indrome <fpschocke@gmail.com>
2019-09-06 14:32:35 +0200

    cinemachine, postprocess stack, skybox rotation
    - updated cinemachine and postprocessing stack package
    - created postprocessing profile for main menu and fixed postprocessing in main menu scene
    - fixed skybox rotation changing material on each play

--- Immerlympia/Assets/Scripts/Camera/SkyboxTurn.cs
+++ Immerlympia/Assets/Scripts/Camera/SkyboxTurn.cs
@@ -8,11 +8,12 @@ public class SkyboxTurn : MonoBehaviour
     [SerializeField] float turnSpeed = 1f;
     [SerializeField, Range(0f, 360f)] float startRotation = 0;
     [SerializeField] GameObject objectSky;
-    float currentRotation;
+    float currentRotation, materialRotationAtStart;
     Material skyboxMaterial;
     void Awake(){
         skyboxMaterial = GetComponent<Skybox>().material;
         currentRotation = startRotation;
+        materialRotationAtStart = skyboxMaterial.GetFloat("_Rotation");
         skyboxMaterial.SetFloat("_Rotation", currentRotation);
         if(objectSky == null)
             objectSky = new GameObject("objectSky empty");
@@ -22,5 +23,9 @@ public class SkyboxTurn : MonoBehaviour
         currentRotation += turnSpeed * Time.deltaTime;
         objectSky.transform.eulerAngles = new Vector3(0, currentRotation, 0);
         skyboxMaterial.SetFloat("_Rotation", currentRotation % 360);
+    }
+
+    void OnDestroy(){
+        skyboxMaterial.SetFloat("_Rotation", materialRotationAtStart);
     }
 }
